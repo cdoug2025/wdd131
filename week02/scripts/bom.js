1cdfc6c4b42c77aa7309c53847b7dafa83972dcc
cdoug2025 <chris5617@hotmail.com>
2026-01-15 17:34:03 -0500

    Completed Week 2 bom Learning Activities

---
+++ week02/scripts/bom.js
@@ -0,0 +1,21 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function() {
+    if (input.value.trim() !== "") {   
+        const li = document.createElement("li")
+        const deleteButton = document.createElement("button")
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        deleteButton.setAttribute("aria-label", "delete")
+        li.append(deleteButton)
+        list.append(li)
+        deleteButton.addEventListener("click", function() {
+            input.focus()
+            list.removeChild(li)
+        })
+    }
+    input.value = '';
+    input.focus()
+})
